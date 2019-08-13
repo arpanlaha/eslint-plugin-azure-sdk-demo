f9fe590485fc2dbdd2d1be756e6780e08090f5d4
Arpan Laha <arpanlaha99@gmail.com>
2019-08-13 11:24:03 -0700

    add demo files

---
+++ v2/src/util/constants.ts
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+export const packageJsonInfo = {
+  name: "@azure/service-bus",
+  version: "1.0.3"
+};
+
+export const messageDispositionTimeout = 20000;
+
+export const max32BitNumber = Math.pow(2, 31) - 1;
